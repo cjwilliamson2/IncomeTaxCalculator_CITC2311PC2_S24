465c52dcd96aa94f96e4cc6dcd6b6ab101c933a2
cjwilliamson2 <cjwilliamson2@MC219T-C>
2024-02-01 10:14:55 -0500

    Add project files.

---
+++ IncomeTaxCalculator/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace IncomeTaxCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnCalculate = new Button();
+            btnExit = new Button();
+            lblTaxableIncome = new Label();
+            lblIncomeTaxOwed = new Label();
+            txtTaxableIncome = new TextBox();
+            txtTaxableIncomeOwed = new TextBox();
+            SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            btnCalculate.Location = new Point(144, 180);
+            btnCalculate.Margin = new Padding(2, 2, 2, 2);
+            btnCalculate.Name = "btnCalculate";
+            btnCalculate.Size = new Size(78, 20);
+            btnCalculate.TabIndex = 1;
+            btnCalculate.Text = "Calculate";
+            btnCalculate.UseVisualStyleBackColor = true;
+            btnCalculate.Click += btnCalculate_Click;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(345, 180);
+            btnExit.Margin = new Padding(2, 2, 2, 2);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(78, 20);
+            btnExit.TabIndex = 2;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // lblTaxableIncome
+            // 
+            lblTaxableIncome.AutoSize = true;
+            lblTaxableIncome.Location = new Point(144, 61);
+            lblTaxableIncome.Margin = new Padding(2, 0, 2, 0);
+            lblTaxableIncome.Name = "lblTaxableIncome";
+            lblTaxableIncome.Size = new Size(92, 15);
+            lblTaxableIncome.TabIndex = 4;
+            lblTaxableIncome.Text = "Taxable Income:";
+            // 
+            // lblIncomeTaxOwed
+            // 
+            lblIncomeTaxOwed.AutoSize = true;
+            lblIncomeTaxOwed.Location = new Point(130, 114);
+            lblIncomeTaxOwed.Margin = new Padding(2, 0, 2, 0);
+            lblIncomeTaxOwed.Name = "lblIncomeTaxOwed";
+            lblIncomeTaxOwed.Size = new Size(104, 15);
+            lblIncomeTaxOwed.TabIndex = 5;
+            lblIncomeTaxOwed.Text = "Income Tax Owed:";
+            // 
+            // txtTaxableIncome
+            // 
+            txtTaxableIncome.Location = new Point(300, 59);
+            txtTaxableIncome.Margin = new Padding(2, 2, 2, 2);
+            txtTaxableIncome.Name = "txtTaxableIncome";
+            txtTaxableIncome.Size = new Size(106, 23);
+            txtTaxableIncome.TabIndex = 0;
+            // 
+            // txtTaxableIncomeOwed
+            // 
+            txtTaxableIncomeOwed.Location = new Point(300, 114);
+            txtTaxableIncomeOwed.Margin = new Padding(2, 2, 2, 2);
+            txtTaxableIncomeOwed.Name = "txtTaxableIncomeOwed";
+            txtTaxableIncomeOwed.ReadOnly = true;
+            txtTaxableIncomeOwed.Size = new Size(106, 23);
+            txtTaxableIncomeOwed.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AcceptButton = btnCalculate;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = btnExit;
+            ClientSize = new Size(560, 270);
+            Controls.Add(txtTaxableIncomeOwed);
+            Controls.Add(txtTaxableIncome);
+            Controls.Add(lblIncomeTaxOwed);
+            Controls.Add(lblTaxableIncome);
+            Controls.Add(btnExit);
+            Controls.Add(btnCalculate);
+            Margin = new Padding(2, 2, 2, 2);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnCalculate;
+        private Button btnExit;
+        private Label lblTaxableIncome;
+        private Label lblIncomeTaxOwed;
+        private TextBox txtTaxableIncome;
+        private TextBox txtTaxableIncomeOwed;
+    }
+}
---
+++ IncomeTaxCalculator/Form1.cs
@@ -0,0 +1,49 @@
+namespace IncomeTaxCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            decimal taxableIncome = Convert.ToDecimal(txtTaxableIncome.Text);
+
+            decimal taxOwed = CalculateTaxBurden(taxableIncome);
+
+            txtTaxableIncomeOwed.Text = taxOwed.ToString();
+        }
+
+        private static decimal CalculateTaxBurden(decimal income)
+        {
+            decimal taxOwed = 0m;
+
+            if (income <= 11000)
+            {
+                taxOwed = income * .10m;
+            }
+            else if (income <= 44735 && income > 11000)
+            {
+                taxOwed = ((income - 11000) * .12m) + 1100;
+            }
+            else if (income <= 95375 && income > 44735)
+            {
+                taxOwed = ((income - 44735) * .22m) + 5147;
+            }
+            else
+            {
+                taxOwed = ((income - 95375) * 1m) + 16290;
+            }
+
+            return taxOwed;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
